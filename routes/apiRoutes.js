03b2a4d456f5f531c8e236aab126bedcb5051fa7
Syed Ahsan <ahsan.ali0307@gmail.com>
2021-07-13 23:06:25 -0400

    get workoutpage route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,15 @@
 const router = require("express").Router();
 const db = require("../models/workout");
 
+router.get("/api/workouts", (req, res) => {
+	db.find()
+		.then((dbData) => {
+			res.json(dbData);
+		})
+		.catch((err) => {
+			res.json(err);
+		});
+});
+
 
 module.exports = router;
