787dded2a42ef6f8e268ba382643d1a32fceecd2
Syed Ahsan <ahsan.ali0307@gmail.com>
2021-07-13 22:31:08 -0400

    homepage router

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,2 +1,7 @@
 const router = require("express").Router();
 const path = require("path");
+
+// Homepage
+router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
